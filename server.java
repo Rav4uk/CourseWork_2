02e6df81abff028545175960e90c3fc40e5b1222
Rav4uk <ravenuss125@gmail.com>
2015-06-05 05:24:21 +0400

    Update server.java

--- server.java
+++ server.java
@@ -1,5 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -15,14 +17,16 @@ import javax.swing.JTextArea;
 public class Server {
     
 	JTextArea area;    
-    Server(){
-        JFrame main_window = new JFrame("Server");
+    Server(){ // интерфейс сервера
+        JFrame main_window = new JFrame("Server"); //создается основное окно
         main_window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         main_window.setSize(200, 300); 
+		//создаем зону для текста
         area = new JTextArea();
 		JScrollPane scroll = new JScrollPane(area);
 		main_window.getContentPane().add(scroll);
 		area.setEditable(false);
+        //main_window.add(area);
         main_window.setVisible(true);
         connect();
         
@@ -33,6 +37,7 @@ public class Server {
 		String answer;
 		
 		area.setText(null);
+        //try - пробуем сделать; связан с catch (ниже)
         try {
             ServerSocket ss = new ServerSocket(port);
             area.append ("Local IP address:\n" + 
@@ -40,15 +45,17 @@ public class Server {
 			int r=0;
 			
             while(true){
+				//area.setText(null); //очистка старого
+				//создаем сокет для подключения
 				area.append("Waiting for connection\n");
                 Socket socket = ss.accept();
-				Process prog;
 				area.append("Connected\n");
+				//создаем входной поток и поток входных данных
                 InputStream in = socket.getInputStream();
 				byte[] str = new byte[64 * 1024];
 				r = in.read(str);
 				String data = new String(str, 0, r);
-                area.append(data+"\n");
+                area.append("Client's message: "+data+"\n");
 				
 				if (data.equals("2"))
 				{
@@ -68,19 +75,38 @@ public class Server {
 				{
 					if (data.equals("3"))
 					{
-						prog = Runtime.getRuntime().exec("kill.bat");
-						answer = "Stream's stopped";
+						if (CheckProcess("ffmpeg.exe"))
+						{
+							Runtime.getRuntime().exec("kill.bat");
+							answer = "Stream's stopped";	
+							area.append("Stream's stopped\n");
+						}
+						else
+						{
+							answer = "There is no stream";
+							area.append("There is no stream\n");
+						}
 						socket.getOutputStream().write(answer.getBytes());
 					}
 					else
 					{
-						try {
-							String prog_name = "stream"+data+".bat";
-							prog = Runtime.getRuntime().exec(prog_name);
-						} catch (Exception e) {
-							System.out.println(e.getMessage());
-						}	
-						answer="Stream's started (" + data + ")";
+						if (!CheckProcess("ffmpeg.exe"))
+						{
+							try {
+								String prog_name = "stream"+data+".bat";
+								Runtime.getRuntime().exec(prog_name);
+								answer="Stream's started (" + data + ")";
+								area.append("Stream's started (" + data + ")\n");
+							} catch (Exception e) {
+								answer="Error in creating stream";
+								area.append("Error in creating stream\n");
+								System.out.println(e.getMessage());
+							}	
+							
+						
+						}
+						else
+							answer="Stream's already started";
 						socket.getOutputStream().write(answer.getBytes());
 					}
 				}
@@ -88,12 +114,16 @@ public class Server {
 				area.setCaretPosition (area.getDocument().getLength());
             }
         }
+		//вывод ошибки если что-то пошло не так
         catch(Exception e){
             e.printStackTrace();
         }
         
 
     }
+    
+
+    
    public static void main(String[] arg){
        new Server();
    }
@@ -118,5 +148,25 @@ public class Server {
         }
     }
     return flag;
-}
+   }
+   StringBuilder fullprocess = new StringBuilder();
+   public boolean CheckProcess(String CmdProcess)
+   {
+	try {
+		Process p = Runtime.getRuntime().exec(System.getenv("windir") + "\\system32\\" + "tasklist.exe");
+		BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String line;
+		while ((line = input.readLine()) != null) 
+		{
+	    		fullprocess.append(line).append("\n");
+		}
+		input.close();
+	} catch (IOException e) {}
+	String process = fullprocess.toString();
+	if(process.contains(CmdProcess))
+		return true;
+	else
+		return false;
+   }
+	   
 }
